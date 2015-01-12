72f48f49fa59d7114920eed6d2430b470e3a80ce
Ifu Aniemeka <ianiemeka@gmail.com>
2015-01-12 14:35:14 -0600

    Allow admin to fetch sent and received challenges

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,8 +1,12 @@
 class Challenge < ActiveRecord::Base
+  enum state: [:pending, :rejected, :accepted, :completed]
+
   belongs_to :post
   belongs_to :sender, class_name: "Circle", foreign_key: "sender_id"
   belongs_to :recipient, class_name: "Circle", foreign_key: "recipient_id"
 
   validates_presence_of :name, :sender_id, :recipient_id
+
+  scope :recently_sent_challenges, -> (circle_id) { where(sender_id: circle_id).where("updated_at > ?", 4.weeks.ago).order(:updated_at) }
+  scope :recently_received_challenges, -> (circle_id) { where(recipient_id: circle_id).where("updated_at > ?", 4.weeks.ago).order(:updated_at) }
 end
-# A challenge can be in one of 3 states: pending, rejected, and accepted
