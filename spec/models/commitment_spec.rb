11b6f0ca45b1b07fcdd38d138ec1439cf34a8662
Ifu Aniemeka <ianiemeka@gmail.com>
2015-01-12 14:36:12 -0600

    Limit number of people that can join a run. WIP

--- spec/models/commitment_spec.rb
+++ spec/models/commitment_spec.rb
@@ -3,14 +3,13 @@ require 'rails_helper'
 RSpec.describe Commitment, :type => :model do
 
   it "has a valid factory" do
-    binding.pry
     expect(create(:commitment)).to be_valid
   end
 
-  xit "is invalid if the max number of runners has been reached" do
+  it "is invalid if the max number of runners has been reached" do
     # binding.pry
     post = create(:post, max_runners: 2)
-    # create_list(:commitment, 1, post_id: post.id)
+    create(:commitment, post_id: post.id)
     expect( build(:commitment, post_id: post.id) ).to_not be_valid
   end
 end
