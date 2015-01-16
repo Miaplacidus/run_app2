ef2da19fe158f2ef14c6ed338496640030ec5a8d
Ifu Aniemeka <ianiemeka@gmail.com>
2015-01-16 16:26:55 -0600

    Challenge model specs and related changes in Circle model

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -5,21 +5,24 @@ class Circle < ActiveRecord::Base
   # A post can be both associated with a circle and marked public so other users can join the circle if they would like to
   # join the run
 
-  before_save do |record|
+  before_create do |record|
     CircleUser.create(user_id: record.admin_id, circle_id: record.id)
     location = Geocoder.coordinates(record.city)
     record.location = "POINT(#{location[1]} #{location[0]})"
   end
 
-  belongs_to :admin, class_name:"User"
+  enum level: ['All/Any Levels', 'Military: 6 min and under/mile', 'Advanced: 6-7 min/mi', 'High Intermediate: 7-8 min/mi', 'Intermediate: 8-9 min/mi', 'Beginner: 9-10 min/mi', 'Jogger: 10-11 min/mi', 'Speedwalker: 11-12 min/mi', 'Sprints: 12+ min/mi']
+
+  belongs_to :admin, class_name: "User"
   has_many :circle_users
-  has_many :users, :through => :circle_users
+  has_many :members, through: :circle_users, source: :user
   has_many :posts
   has_many :join_requests
-  has_many :sent_challenges, class_name: "Challenge", foreign_key:"sender_id"
-  has_many :received_challenges, class_name: "Challenge", foreign_key:"recipient_id"
+  has_many :sent_challenges, class_name: "Challenge", foreign_key: "sender_id"
+  has_many :received_challenges, class_name: "Challenge", foreign_key: "recipient_id"
 
   validates_uniqueness_of :name
+  validate :max_members_reached?
 
   scope :get_user_circles, -> (user_id) { where(id: CircleUsers.where(user_id: user_id).map { |circle_user| circle_user.circle_id }) }
   scope :get_admin_circles, -> (user_id) { where(admin_id: user_id) }
@@ -27,5 +30,9 @@ class Circle < ActiveRecord::Base
   scope :filter_by_location, -> (filters) { where("ST_Distance(location, 'POINT(? ?)') < ?", filters[:user_lon].to_f, filters[:user_lat].to_f, filters[:radius].to_f*miles_to_meters) }
   scope :filter_by_full, -> (filters) { select { |circle| CircleUsers.where(circle_id: circle.id).count < circle.max_members }.filter_by_location(filters) }
 
+  private
+  def max_members_reached?
+    self.members.count <= 100
+  end
 end
 
--- spec/models/challenge_spec.rb
+++ spec/models/challenge_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Challenge, :type => :model do
 
+  it "has a valid factory" do
+    expect(build(:challenge)).to be_valid
+  end
+
   it "is invalid without a name" do
     expect( build(:challenge, name: nil) ).to_not be_valid
   end
@@ -12,5 +16,15 @@ RSpec.describe Challenge, :type => :model do
 
   it "is invalid without a recipient" do
     expect( build(:challenge, recipient_id:nil) ).to_not be_valid
+  end
+
+  it "gets recently sent challenges" do
+    sending_circle_id = create(:challenge).sender_id
+    expect(Challenge.recently_sent_challenges(sending_circle_id).count).to eq 1
+  end
+
+  it "gets recently received challenges" do
+    receiving_circle_id = create(:challenge).recipient_id
+    expect(Challenge.recently_received_challenges(receiving_circle_id).count).to eq 1
   end
 end
