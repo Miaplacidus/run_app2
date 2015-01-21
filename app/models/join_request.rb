b355e9fc493368b4fe3338f2b2f61f4f7ff03798
Ifu Aniemeka <ianiemeka@gmail.com>
2015-01-21 13:02:15 -0600

    Join Request model specs

--- app/models/join_request.rb
+++ app/models/join_request.rb
@@ -1,4 +1,8 @@
 class JoinRequest < ActiveRecord::Base
+  enum state: [:pending, :rejected, :accepted]
+
+  validates_presence_of :circle_id, :user_id
+
   belongs_to :user
   belongs_to :circle
 end
