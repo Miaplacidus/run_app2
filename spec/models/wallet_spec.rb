dcf5ed2f063764200c7b0d035b2d13e75edd1878
Ifu Aniemeka <ianiemeka@gmail.com>
2015-01-21 16:06:14 -0600

    Minor changes to wallet spec

--- spec/models/wallet_spec.rb
+++ spec/models/wallet_spec.rb
@@ -2,7 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Wallet, :type => :model do
 
-  it "is invalid if balance drops below zero" do
-    expect(build(:wallet, balance: -1.0)).to_not be_valid
+  it "has a valid factory" do
+    expect(create(:wallet)).to be_valid
+  end
+
+  describe "validations" do
+    it "is invalid if balance drops below zero" do
+      expect(build(:wallet, balance: -1.0)).to_not be_valid
+    end
   end
 end
